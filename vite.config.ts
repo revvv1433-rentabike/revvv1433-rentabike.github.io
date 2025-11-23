60e21986b9462e960bbf1703802b37fcfd6750d3
revvv1433 <revvv1433@gmail.com>
2025-11-23 23:07:59 +0530

    some changes in url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: "https://www.tirupathibikerentals.com/",
+  base: "https://tirupathibikerentals.com/",
 
   optimizeDeps: {
     exclude: ['lucide-react'],
